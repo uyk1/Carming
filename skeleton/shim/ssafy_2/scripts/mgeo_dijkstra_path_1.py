792c83396dd3702f9c2565c1f9bfd5ddf3283174
심정윤 <shimmumu@naver.com>
2023-03-13 17:12:53 +0900

    refactor

--- skeleton/shim/ssafy_2/scripts/mgeo_dijkstra_path_1.py
+++ skeleton/shim/ssafy_2/scripts/mgeo_dijkstra_path_1.py
@@ -9,9 +9,9 @@ import copy
 import numpy as np
 import json
 
-from math import cos,sin,sqrt,pow,atan2,pi
-from geometry_msgs.msg import Point32,PoseStamped
-from nav_msgs.msg import Odometry,Path
+from math import cos, sin, sqrt, pow, atan2, pi
+from geometry_msgs.msg import Point32, PoseStamped
+from nav_msgs.msg import Odometry, Path
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(current_path)
@@ -35,7 +35,7 @@ from lib.mgeo.class_defs import *
 # 10. dijkstra 경로 데이터를 ROS Path 메세지 형식에 맞춰 정의
 # 11. dijkstra 이용해 만든 Global Path 정보 Publish
 
-#TODO: (0) 필수 학습 지식
+# TODO: (0) 필수 학습 지식
 '''
 # dijkstra 알고리즘은 그래프 구조에서 노드 간 최단 경로를 찾는 알고리즘 입니다.
 # 시작 노드부터 다른 모든 노드까지의 최단 경로를 탐색합니다.
@@ -52,89 +52,94 @@ from lib.mgeo.class_defs import *
 # 
 
 '''
-class dijkstra_path_pub :
+
+
+class dijkstra_path_pub:
     def __init__(self):
         rospy.init_node('dijkstra_path_pub', anonymous=True)
 
-        self.global_path_pub = rospy.Publisher('/global_path',Path, queue_size = 1)
+        self.global_path_pub = rospy.Publisher('/global_path', Path, queue_size=1)
 
-        #TODO: (1) Mgeo data 읽어온 후 데이터 확인
+        # TODO: (1) Mgeo data 읽어온 후 데이터 확인
         load_path = os.path.normpath(os.path.join(current_path, 'lib/mgeo_data/R_KR_PG_K-City'))
         mgeo_planner_map = MGeo.create_instance_from_json(load_path)
 
         node_set = mgeo_planner_map.node_set
         link_set = mgeo_planner_map.link_set
 
-        self.nodes=node_set.nodes
-        self.links=link_set.lines
+        self.nodes = node_set.nodes
+        self.links = link_set.lines
 
-        self.global_planner=Dijkstra(self.nodes,self.links)
+        self.global_planner = Dijkstra(self.nodes, self.links)
 
-        #TODO: (2) 시작 Node 와 종료 Node 정의
+        # TODO: (2) 시작 Node 와 종료 Node 정의
         '''
         # Dijkstra Path 를 만들기 위한 출발 Node 와 도착 Node의 Idx 를 지정합니다.
         # MGeo 데이터는 Json 파일로 Idx 정보를 확인 할 수 있지만 시뮬레이터를 통해 직접 확인 가능합니다.
         # F8 키보드 입력 또는 시뮬레이터에서 화면 좌측 상단에 View --> MGeo Viewer 를 클릭합니다.
         # MGeo Viewer 기능을 이용하여 맵에있는 MGeo 정보를 확인 할 수 있으며 시각화 까지 가능합니다.
         # 해당 기능을 이용하여 원하는 시작 위치와 종료 위치의 Node 이름을 알아낸 뒤 아래 변수에 입력하세요.
-        
+        '''
+
         self.start_node = 'A119BS010184'
         self.end_node = 'A119BS010148'
-
-        '''
 
         self.global_path_msg = Path()
         self.global_path_msg.header.frame_id = '/map'
 
         self.global_path_msg = self.calc_dijkstra_path_node(self.start_node, self.end_node)
 
-        rate = rospy.Rate(10) # 10hz
+        rate = rospy.Rate(10)  # 10hz
         while not rospy.is_shutdown():
-            #TODO: (11) dijkstra 이용해 만든 Global Path 정보 Publish
+            # TODO: (11) dijkstra 이용해 만든 Global Path 정보 Publish
             '''
             # dijkstra 이용해 만든 Global Path 메세지 를 전송하는 publisher 를 만든다.
-            self.global_path_pub.
-            
             '''
+            self.global_path_pub.publish(self.global_path_msg)
             rate.sleep()
 
     def calc_dijkstra_path_node(self, start_node, end_node):
 
         result, path = self.global_planner.find_shortest_path(start_node, end_node)
 
-        #TODO: (10) dijkstra 경로 데이터를 ROS Path 메세지 형식에 맞춰 정의
+        # TODO: (10) dijkstra 경로 데이터를 ROS Path 메세지 형식에 맞춰 정의
         out_path = Path()
         out_path.header.frame_id = '/map'
         '''
         # dijkstra 경로 데이터 중 Point 정보를 이용하여 Path 데이터를 만들어 줍니다.
-
         '''
+        for point in path.get('point_path'):
+            pose = PoseStamped()
+            pose.pose.position.x = point[0]
+            pose.pose.position.y = point[1]
+            out_path.poses.append(pose)
 
         return out_path
+
 
 class Dijkstra:
     def __init__(self, nodes, links):
         self.nodes = nodes
         self.links = links
-        #TODO: (3) weight 값 계산
+        # TODO: (3) weight 값 계산
         self.weight = self.get_weight_matrix()
         self.lane_change_link_idx = []
 
     def get_weight_matrix(self):
-        #TODO: (3) weight 값 계산
+        # TODO: (3) weight 값 계산
         '''
         # weight 값 계산은 각 Node 에서 인접 한 다른 Node 까지의 비용을 계산합니다.
         # 계산된 weight 값 은 각 노드간 이동시 발생하는 비용(거리)을 가지고 있기 때문에
         # Dijkstra 탐색에서 중요하게 사용 됩니다.
         # weight 값은 딕셔너리 형태로 사용 합니다.
-        # 이중 중첩된 딕셔너리 형태로 사용하며 
+        # 이중 중첩된 딕셔너리 형태로 사용하며
         # Key 값으로 Node의 Idx Value 값으로 다른 노드 까지의 비용을 가지도록 합니다.
-        # 아래 코드 중 self.find_shortest_link_leading_to_node 를 완성하여 
+        # 아래 코드 중 self.find_shortest_link_leading_to_node 를 완성하여
         # Dijkstra 알고리즘 계산을 위한 Node와 Node 사이의 최단 거리를 계산합니다.
-
         '''
+
         # 초기 설정
-        weight = dict() 
+        weight = dict()
         for from_node_id, from_node in self.nodes.items():
             # 현재 노드에서 다른 노드로 진행하는 모든 weight
             weight_from_this_node = dict()
@@ -149,60 +154,59 @@ class Dijkstra:
 
             for to_node in from_node.get_to_nodes():
                 # 현재 노드에서 to_node로 연결되어 있는 링크를 찾고, 그 중에서 가장 빠른 링크를 찾아준다
-                shortest_link, min_cost = self.find_shortest_link_leading_to_node(from_node,to_node)
-                weight[from_node_id][to_node.idx] = min_cost           
+                shortest_link, min_cost = self.find_shortest_link_leading_to_node(from_node, to_node)
+                weight[from_node_id][to_node.idx] = min_cost
 
         return weight
 
-    def find_shortest_link_leading_to_node(self, from_node,to_node):
+    def find_shortest_link_leading_to_node(self, from_node, to_node):
         """현재 노드에서 to_node로 연결되어 있는 링크를 찾고, 그 중에서 가장 빠른 링크를 찾아준다"""
-        #TODO: (3) weight 값 계산
+        # TODO: (3) weight 값 계산
         '''
         # 최단거리 Link 인 shortest_link 변수와
         # shortest_link 의 min_cost 를 계산 합니다.
-
         '''
-
+        shortest_link, min_cost = from_node.find_shortest_link_leading_to_node(to_node)
         return shortest_link, min_cost
-        
-    def find_nearest_node_idx(self, distance, s):        
+
+    def find_nearest_node_idx(self, distance, s):
         idx_list = self.nodes.keys()
         min_value = float('inf')
         min_idx = idx_list[-1]
 
         for idx in idx_list:
-            if distance[idx] < min_value and s[idx] == False :
+            if distance[idx] < min_value and s[idx] == False:
                 min_value = distance[idx]
                 min_idx = idx
         return min_idx
 
-    def find_shortest_path(self, start_node_idx, end_node_idx): 
-        #TODO: (4) Dijkstra Path 초기화 로직
+    def find_shortest_path(self, start_node_idx, end_node_idx):
+        # TODO: (4) Dijkstra Path 초기화 로직
         # s 초기화         >> s = [False] * len(self.nodes)
         # from_node 초기화 >> from_node = [start_node_idx] * len(self.nodes)
         '''
         # Dijkstra 경로 탐색을 위한 초기화 로직 입니다.
-        # 변수 s와 from_node 는 딕셔너리 형태로 크기를 MGeo의 Node 의 개수로 설정합니다. 
+        # 변수 s와 from_node 는 딕셔너리 형태로 크기를 MGeo의 Node 의 개수로 설정합니다.
         # Dijkstra 알고리즘으로 탐색 한 Node 는 변수 s 에 True 로 탐색하지 않은 변수는 False 로 합니다.
         # from_node 의 Key 값은 Node 의 Idx로
         # from_node 의 Value 값은 Key 값의 Node Idx 에서 가장 비용이 작은(가장 가까운) Node Idx로 합니다.
         # from_node 통해 각 Node 에서 가장 가까운 Node 찾고
-        # 이를 연결해 시작 노드부터 도착 노드 까지의 최단 경로를 탐색합니다. 
+        # 이를 연결해 시작 노드부터 도착 노드 까지의 최단 경로를 탐색합니다.
 
         '''
         s = dict()
-        from_node = dict() 
+        from_node = dict()
         for node_id in self.nodes.keys():
             s[node_id] = False
             from_node[node_id] = start_node_idx
 
         s[start_node_idx] = True
-        distance =copy.deepcopy(self.weight[start_node_idx])
+        distance = copy.deepcopy(self.weight[start_node_idx])
 
-        #TODO: (5) Dijkstra 핵심 코드
+        # TODO: (5) Dijkstra 핵심 코드
         for i in range(len(self.nodes.keys()) - 1):
             selected_node_idx = self.find_nearest_node_idx(distance, s)
-            s[selected_node_idx] = True            
+            s[selected_node_idx] = True
             for j, to_node_idx in enumerate(self.nodes.keys()):
                 if s[to_node_idx] == False:
                     distance_candidate = distance[selected_node_idx] + self.weight[selected_node_idx][to_node_idx]
@@ -210,17 +214,17 @@ class Dijkstra:
                         distance[to_node_idx] = distance_candidate
                         from_node[to_node_idx] = selected_node_idx
 
-        #TODO: (6) node path 생성
+        # TODO: (6) node path 생성
         tracking_idx = end_node_idx
         node_path = [end_node_idx]
-        
+
         while start_node_idx != tracking_idx:
             tracking_idx = from_node[tracking_idx]
-            node_path.append(tracking_idx)     
+            node_path.append(tracking_idx)
 
         node_path.reverse()
 
-        #TODO: (7) link path 생성
+        # TODO: (7) link path 생성
         link_path = []
         for i in range(len(node_path) - 1):
             from_node_idx = node_path[i]
@@ -229,22 +233,22 @@ class Dijkstra:
             from_node = self.nodes[from_node_idx]
             to_node = self.nodes[to_node_idx]
 
-            shortest_link, min_cost = self.find_shortest_link_leading_to_node(from_node,to_node)
+            shortest_link, min_cost = self.find_shortest_link_leading_to_node(from_node, to_node)
             link_path.append(shortest_link.idx)
 
-        #TODO: (8) Result 판별
+        # TODO: (8) Result 판별
         if len(link_path) == 0:
-            return False, {'node_path': node_path, 'link_path':link_path, 'point_path':[]}
+            return False, {'node_path': node_path, 'link_path': link_path, 'point_path': []}
 
-        #TODO: (9) point path 생성
-        point_path = []        
+        # TODO: (9) point path 생성
+        point_path = []
         for link_id in link_path:
             link = self.links[link_id]
             for point in link.points:
                 point_path.append([point[0], point[1], 0])
 
-        return True, {'node_path': node_path, 'link_path':link_path, 'point_path':point_path}
+        return True, {'node_path': node_path, 'link_path': link_path, 'point_path': point_path}
+
 
 if __name__ == '__main__':
-    
     dijkstra_path_pub = dijkstra_path_pub()
