11488424c8c59765d7edbdf88f3c3c1ddbb16d58
unknown <06___what@naver.com>
2023-03-20 16:42:55 +0900

    Feat: Image_parser_hsv.py
    RGB채널을 HSV채널로 바꾸기
    
    Related to: #S08P22A408-167

--- ssafy_3/scripts/image_parser_hsv.py
+++ ssafy_3/scripts/image_parser_hsv.py
@@ -24,41 +24,40 @@ class IMGParser:
 
     def callback(self, msg):
         try:
-            '''
-
-            np_arr = np.fromstring(             )
-            img_bgr = cv2.imdecode(             )
-
-            '''
+            np_arr = np.fromstring(msg.data, dtype=np.uint8, count=-1)
+            img_bgr = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            
         except CvBridgeError as e:
             print(e)
 
         #TODO: (1)
-        '''
+        
         # cv2.cvtColor 함수를 이용하여 bgr 이미지를 hsv 이미지로 변환합니다.
         # cv2.cvtColor 함수는 색상 공간 변환 함수로 이미지를 원하는 색상 코드로 변환합니다.
         # 이번 예제에서는 bgr이미지를 HSV 이미지로 변환합니다.
 
-        img_hsv = cv2.cvtColor(                 )
+        img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+        ## cv2.cvtColor(변환하고자 하는 이미지, 색변환 코드)
 
-        '''
+        
         #TODO: (2)
-        '''
+        
         # np.concatenate 함수는 는 서로 다른 두 배열을 합치는 예제입니다.
         # bgr 이미지와 hsv 이미지를 동시에 출력하기 위해 np.concatenate 함수를 사용해 이미지를 합칩니다.
 
-        img_concat = np.concatenate(            )
-
-        '''
+        img_concat = np.concatenate((img_hsv, img_bgr), axis=1)
+        ## axis = 1 : np.concatenate()를 사용해서 배열을 연결할 때, 수평으로 연결하라는 의미
+        ## axis = 0 : 수직으로 연결
+        
 
         #TODO: (3)
-        '''
+        
         # 이미지를 출력 합니다.
 
-        cv2.imshow(         )
-        cv2.waitKey(        ) 
+        cv2.imshow('image_concat', img_bgr)
+        cv2.waitKey(1)
 
-        '''
+        
 
 
 if __name__ == '__main__':
