d650c5c15c83bf74b8383d7e779dda94d3a39eb3
ShinKwang <sklee0206@naver.com>
2023-03-20 16:35:34 +0900

    feat: Make log aop for controller

--- backend/src/main/java/com/carming/backend/common/aop/timer/Timer.java
+++
@@ -1,9 +0,0 @@
-package com.carming.backend.common.aop.timer;
-
-import org.aspectj.lang.ProceedingJoinPoint;
-
-public interface Timer {
-    Long start();
-
-    Long getElapsedTime(Long startTime);
-}
