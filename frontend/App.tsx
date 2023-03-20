b4a1dcc4f1487e511f9ca9f0d8357c16446c8d9f
김인중 <qwe_asd13579@naver.com>
2023-03-20 16:29:04 +0900

    style: React Native 테마 적용
    Related to: #S08P22A408-174

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -7,12 +7,29 @@
 
 import {NavigationContainer} from '@react-navigation/native';
 import L1_RootStackNavigator from './src/navigations/L1_RootStackNavigator';
+import {MD3LightTheme, Provider as PaperProvider} from 'react-native-paper';
+
+const theme = {
+  ...MD3LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    primary: '#7173C9',
+    secondary: '#DF94C2',
+    tertiary: '#FFBDC1',
+    quaternary: '#141060',
+    surface: '#8398D1',
+    accent: '#70558E',
+    neutral: '#D9D9D9',
+  },
+};
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <L1_RootStackNavigator />
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <L1_RootStackNavigator />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
