940bb21650f8bea889a98bd5e39b6b2ab26cb786
ShinKwang <sklee0206@naver.com>
2023-03-21 17:14:20 +0900

    test: Add test for value annotation

--- backend/src/test/java/com/carming/backend/externalApi/naver/sms/SmsFactoryTest.java
+++ backend/src/test/java/com/carming/backend/externalApi/naver/sms/SmsFactoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class SmsFactoryTest {
 
@@ -12,15 +14,12 @@ class SmsFactoryTest {
     SmsFactory factory;
 
     @Test
-    @DisplayName("")
+    @DisplayName("Value 애노테이션 값이 잘 들어가는지 확인")
     void makeSmsFactory() {
-
-        factory.send("01051391314", "123456");
-
-        System.out.println(factory.getServiceId());
-        System.out.println(factory.getSecretKey());
-        System.out.println(factory.getAccessKey());
-        System.out.println(factory.getWebClient());
+        assertThat(factory.getAccessKey()).isNotNull();
+        assertThat(factory.getSecretKey()).isNotNull();
+        assertThat(factory.getServiceId()).isNotNull();
+        assertThat(factory.getWebClient()).isNull();
     }
 
 }
