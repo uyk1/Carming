abf4435378affe6196d51dafe7b50b353e89f068
김인중 <qwe_asd13579@naver.com>
2023-03-23 11:04:23 +0900

    refactor: 장소 카테고리 enum 값 설정
    related to: #S08P22A408-180

--- frontend/src/types/index.tsx
+++ frontend/src/types/index.tsx
@@ -8,7 +8,19 @@ type Place = {
   color: string;
   title: string;
   content: string;
+  location: string;
   imageUrl: string;
+  ratingSum: number;
+  ratingCnt: number;
 };
 
+enum Category {
+  음식점 = 'FOOD',
+  카페 = 'CAFE',
+  놀거리 = 'PLAY',
+  명소 = 'ATTRACTION',
+  숙박 = 'ACCOMODATION',
+}
+
 export type {Tag, Place};
+export {Category};
