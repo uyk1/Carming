7ee86ef720916688893f08d16a293f1c42a9674f
ShinKwang <sklee0206@naver.com>
2023-03-23 13:51:30 +0900

    test: Add test for JsonMapper

---
+++ backend/src/test/java/com/carming/backend/common/JsonMapperTest.java
@@ -0,0 +1,21 @@
+package com.carming.backend.common;
+
+import com.carming.backend.member.domain.valid.AuthenticationInfo;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonMapperTest {
+
+    @Test
+    @DisplayName("JSON 변환 확인")
+    void convertToJSON() {
+        final String PHONE_NUMBER = "01051391314";
+        String authInfo = JsonMapper.toJson(new AuthenticationInfo(PHONE_NUMBER, true));
+        AuthenticationInfo information = JsonMapper.toClass(authInfo, AuthenticationInfo.class);
+
+        System.out.println(authInfo);
+        System.out.println(information);
+    }
+}
