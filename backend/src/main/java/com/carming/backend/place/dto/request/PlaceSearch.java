9bffce9d956f6b5bcfa95d755ec72a2f4256a55d
ShinKwang <sklee0206@naver.com>
2023-03-24 10:40:26 +0900

    feat: Add getPlaces feature

--- backend/src/main/java/com/carming/backend/place/dto/request/PlaceSearch.java
+++ backend/src/main/java/com/carming/backend/place/dto/request/PlaceSearch.java
@@ -1,6 +1,7 @@
 package com.carming.backend.place.dto.request;
 
 import com.carming.backend.place.domain.PlaceCategory;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,9 +11,26 @@ import java.util.List;
 @Data
 public class PlaceSearch {
 
+    private final static Long DEFAULT_SIZE = 50L;
+
     private List<String> regions;
 
-    private PlaceCategory category;
+    private String category;
+
+    private Long size;
 
 
+    @Builder
+    public PlaceSearch(List<String> regions, String category, Long size) {
+        this.regions = regions;
+        this.category = category;
+        this.size = validNull(size);
+    }
+
+    private Long validNull(Long size) {
+        if (size == null) {
+            size = DEFAULT_SIZE;
+        }
+        return size;
+    }
 }
