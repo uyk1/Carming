aa64bc5e792e9f037d1c95752fce2c24cc157bef
ShinKwang <sklee0206@naver.com>
2023-03-27 17:41:49 +0900

    refactor: Add uri in requestMapping of login controller

--- backend/src/main/java/com/carming/backend/login/controller/LoginController.java
+++ backend/src/main/java/com/carming/backend/login/controller/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 @RestController
 public class LoginController {
 
