b24953371bf54a0547ba97d45be76d4043fe8db6
심정윤 <shimmumu@naver.com>
2023-03-27 15:52:47 +0900

    Fix
    Added 레디스에서 받은 데이터로 문열림닫힘 구현, main에서 동작

--- RC_Car_control/Redis_get.py
+++ RC_Car_control/Redis_get.py
@@ -19,19 +19,30 @@ import redis
 ## Redis 클라이언트 객체 생성
 ## port와 password 인자를 사용하여 서버 연결 정보 지정
 ## db는 데이터베이스 번호, 기본값은 0
+def get_data():
 
-try:
-  
-  redis_client = redis.StrictRedis(host='j8a408.p.ssafy.io', port=6379, db=0)
+  try:
+    redis_client = redis.StrictRedis(host='j8a408.p.ssafy.io', port=6379, db=0)
 
+    while True:
+      # current_velocity_x = redis_client.get('current_velocity')
+      # current_brake = redis_client.get('current_brake')
+      # destination = redis_client.get('destination')
+      # current_acceleration = redis_client.get('current_acceleration')
+      # current_position_x = redis_client.get('current_position_x')
+      # current_position_y = redis_client.get('current_position_y')
+      hi = 1234
+      hello = 1515
+      return [hi, hello]
 
-  values = redis_client.get('current_velocity')
-  print(values)
+    # return [current_velocity_x, current_brake, destination,
+    #         current_acceleration, current_position_x, current_position_y]
 
-except Exception as ex:
-  print('Error:' ,ex)
-## 연결 종료
-## redis_client.close()
+  except Exception as ex:
+    print('Error:' ,ex)
+  ## 연결 종료
+  ## redis_client.close()
+
 
     
 
--- RC_Car_control/main.py
+++ RC_Car_control/main.py
@@ -3,24 +3,31 @@ import threading
 import os
 import sys
 import socket
-from DC_motor import DC_MOTOR
-from servo_motor import SERVO_MOTOR
-from Door_ServoMotor import DOOR
+from Door_ServoMotor import openclose
+import Redis_get
+
+
 class main():
     def __init__(self):
         pass
 
+    ## 계속 실행시켜야함
+    ## [속도,정지,목적지도달,가속도,현재위치x,현재위치y]
 
+    current_state = Redis_get.get_data()
+    if current_state[1] == 1:
+        print(current_state[1])
+        openclose()
 
 
 if __name__ == "__main__":
     ## Global Variable 선언
     global input_1, input_2, enable, servo_pin
     global steering, drive  ## 각각 조향/주행 함수
-    
     input_1 = 15
     input_2 = 18
     enable = 27
     servo_pin = 17
-    
-    pass
+    current_state = []
+    main()
+
