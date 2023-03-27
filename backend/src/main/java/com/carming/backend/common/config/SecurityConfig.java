32dcf9fea110d4df1d1f246dc5b0be4537029ce3
ShinKwang <sklee0206@naver.com>
2023-03-27 17:42:34 +0900

    refactor: Change antMatcher in spring security

--- backend/src/main/java/com/carming/backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/carming/backend/common/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .exceptionHandling().authenticationEntryPoint(new AuthEntryPoint())
         .and()
                 .authorizeRequests()
-                        .antMatchers("/api/member/**", "/api/login", "/api/logout").permitAll()
+                        .antMatchers("/api/member/**", "/api/auth/**").permitAll()
                         .anyRequest().authenticated();
 
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
@@ -37,6 +37,10 @@ public class SecurityConfig {
     }
 
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
