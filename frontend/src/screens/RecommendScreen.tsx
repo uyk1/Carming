f6f3569495be269f2dc9418154ce3caa02998fef
김인중 <qwe_asd13579@naver.com>
2023-03-30 14:18:10 +0900

    refactor: 추천화면 ScreenProps 적용
    related to: #S08P22A408-322

--- frontend/src/screens/RecommendScreen.tsx
+++ frontend/src/screens/RecommendScreen.tsx
@@ -11,6 +11,119 @@ import CoursesRecommendScreen from './CoursesRecommendScreen';
 import {setPlaceList, setPlaceTagList} from '../redux/slices/placeSlice';
 import {setCourseList, setCourseTagList} from '../redux/slices/courseSlice';
 import {Tag, Place, Category, Course} from '../types';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {L4_CourseCreateStackParamList} from '../navigations/L4_CourseCreateStackNavigator';
+import {CustomButton} from '../components';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+
+export type RecommendScreenProps = NativeStackScreenProps<
+  L4_CourseCreateStackParamList,
+  'Recommend'
+>;
+
+const RecommendScreen: React.FC<RecommendScreenProps> = ({
+  navigation,
+  route,
+}) => {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+
+  const [recommendType, setRecommendType] = useState<string>('0');
+  const recommendTypeChangeButtons = [
+    {
+      value: '0',
+      label: '장소',
+      icon: 'map-marker',
+      checkedColor: 'white',
+      uncheckedColor: 'white',
+      onPress: () => {},
+      style: {
+        borderRadius: 10,
+        backgroundColor:
+          recommendType === '0' ? theme.colors.primary : theme.colors.shadow,
+      },
+    },
+    {
+      value: '1',
+      label: '코스',
+      icon: 'routes',
+      checkedColor: 'white',
+      uncheckedColor: 'white',
+      onPress: () => {},
+      style: {
+        borderRadius: 10,
+        backgroundColor:
+          recommendType === '1' ? theme.colors.primary : theme.colors.shadow,
+      },
+    },
+  ];
+
+  useEffect(() => {
+    dispatch(setPlaceList(places));
+    dispatch(setPlaceTagList(tags));
+    dispatch(setCourseList(courses));
+    dispatch(setCourseTagList(tags));
+  }, [dispatch]);
+
+  return (
+    <AlertNotificationRoot theme={'light'}>
+      <GradientBackground colors={['#70558e7a', '#df94c283', '#ffbdc1b0']}>
+        <SafeAreaView style={{flex: 1}}>
+          <StyledView style={{justifyContent: 'space-between'}}>
+            <SegmentedButtons
+              style={{width: 200}}
+              value={recommendType}
+              onValueChange={setRecommendType}
+              buttons={recommendTypeChangeButtons}
+            />
+            <IconButton
+              icon="home"
+              size={30}
+              onPress={() => {
+                console.log('hello');
+              }}
+            />
+          </StyledView>
+          {recommendType === '0' ? (
+            <PlacesRecommendScreen />
+          ) : (
+            <CoursesRecommendScreen />
+          )}
+          <StyledView style={{justifyContent: 'center'}}>
+            <CustomButton
+              text={'선택 완료'}
+              onPress={() => navigation.navigate('CourseEdit', {recommendType})}
+              buttonStyle={{
+                width: 200,
+                padding: 14,
+                borderRadius: 30,
+                backgroundColor: theme.colors.surfaceVariant,
+              }}
+              textStyle={{
+                fontWeight: 'bold',
+                fontSize: 16,
+                textAlign: 'center',
+              }}
+            />
+          </StyledView>
+        </SafeAreaView>
+      </GradientBackground>
+    </AlertNotificationRoot>
+  );
+};
+
+const StyledView = styled(View)`
+  align-items: center;
+  flex-direction: row;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+const GradientBackground = styled(LinearGradient)`
+  flex: 1;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
 
 const tags: Tag[] = [
   {
@@ -45,10 +158,10 @@ const places: Place[] = [
   },
   {
     id: 1,
-    name: '허니치즈 순대국',
+    name: '파리 엉터리 생고기',
     image: 'https://i.imgur.com/UPrs1EWl.jpg',
-    ratingSum: 17,
-    ratingCount: 4,
+    ratingSum: 222,
+    ratingCount: 80,
     region: '노원구 중계 14동',
     lon: 126.98197125,
     lat: 37.58459777,
@@ -58,10 +171,10 @@ const places: Place[] = [
   },
   {
     id: 2,
-    name: '허니치즈 순대국',
+    name: '경복궁',
     image: 'https://i.imgur.com/MABUbpDl.jpg',
-    ratingSum: 17,
-    ratingCount: 4,
+    ratingSum: 12,
+    ratingCount: 3,
     region: '노원구 중계 14동',
     lon: 127.00569602,
     lat: 37.57033808,
@@ -71,10 +184,10 @@ const places: Place[] = [
   },
   {
     id: 3,
-    name: '허니치즈 순대국',
+    name: '상파울로 엽기 떡볶이',
     image: 'https://i.imgur.com/KZsmUi2l.jpg',
-    ratingSum: 17,
-    ratingCount: 4,
+    ratingSum: 2763,
+    ratingCount: 839,
     region: '노원구 중계 14동',
     lon: 126.98978922,
     lat: 37.60409672,
@@ -117,89 +230,5 @@ const courses: Course[] = [
     ratingSum: 74,
   },
 ];
-
-const RecommendScreen = () => {
-  const theme = useTheme();
-  const dispatch = useDispatch();
-
-  const [recommendType, setRecommendType] = useState<string>('0');
-  const recommendTypeChangeButtons = [
-    {
-      value: '0',
-      label: '장소',
-      icon: 'map-marker',
-      checkedColor: 'white',
-      uncheckedColor: 'white',
-      onPress: () => {},
-      style: {
-        borderRadius: 10,
-        backgroundColor:
-          recommendType === '0' ? theme.colors.primary : theme.colors.shadow,
-      },
-    },
-    {
-      value: '1',
-      label: '코스',
-      icon: 'routes',
-      checkedColor: 'white',
-      uncheckedColor: 'white',
-      onPress: () => {},
-      style: {
-        borderRadius: 10,
-        backgroundColor:
-          recommendType === '1' ? theme.colors.primary : theme.colors.shadow,
-      },
-    },
-  ];
-
-  useEffect(() => {
-    dispatch(setPlaceList(places));
-    dispatch(setPlaceTagList(tags));
-    dispatch(setCourseList(courses));
-    dispatch(setCourseTagList(tags));
-  }, []);
-
-  return (
-    <AlertNotificationRoot theme={'light'}>
-      <GradientBackground colors={['#70558e7a', '#df94c283', '#ffbdc1b0']}>
-        <SafeAreaView style={{flex: 1}}>
-          <StyledView style={{justifyContent: 'space-between'}}>
-            <SegmentedButtons
-              style={{width: 200}}
-              value={recommendType}
-              onValueChange={setRecommendType}
-              buttons={recommendTypeChangeButtons}
-            />
-            <IconButton
-              icon="home"
-              size={30}
-              onPress={() => {
-                console.log('hello');
-              }}
-            />
-          </StyledView>
-          {recommendType === '0' ? (
-            <PlacesRecommendScreen />
-          ) : (
-            <CoursesRecommendScreen />
-          )}
-        </SafeAreaView>
-      </GradientBackground>
-    </AlertNotificationRoot>
-  );
-};
-
-const StyledView = styled(View)`
-  align-items: center;
-  flex-direction: row;
-  padding-left: 20px;
-  padding-right: 20px;
-`;
-
-const GradientBackground = styled(LinearGradient)`
-  flex: 1;
-  padding-top: 20px;
-  padding-bottom: 20px;
-`;
 
 export default RecommendScreen;
