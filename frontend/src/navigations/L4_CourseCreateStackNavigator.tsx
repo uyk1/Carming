df44ffb8429e040e8f61c7a7ad250dadf28770ff
김인중 <qwe_asd13579@naver.com>
2023-03-30 17:25:00 +0900

    refactor: 코스 생성 네비게이션 구조 변경
    related to: #S08P22A408-323

--- frontend/src/navigations/L4_CourseCreateStackNavigator.tsx
+++ frontend/src/navigations/L4_CourseCreateStackNavigator.tsx
@@ -1,10 +1,10 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import CoursesRecommendScreen from '../screens/CoursesRecommendScreen';
-import PlacesRecommendScreen from '../screens/PlacesRecommendScreen';
+import RecommendScreen from '../screens/RecommendScreen';
+import CourseEditScreen from '../screens/CourseEditScreen';
 
 export type L4_CourseCreateStackParamList = {
-  Places: undefined;
-  Courses: undefined;
+  Recommend: undefined;
+  CourseEdit: {recommendType: string};
 };
 
 const Stack = createNativeStackNavigator<L4_CourseCreateStackParamList>();
@@ -12,12 +12,12 @@ const Stack = createNativeStackNavigator<L4_CourseCreateStackParamList>();
 function L4_CourseCreateStackNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName="Places"
+      initialRouteName="Recommend"
       screenOptions={{
         headerShown: false,
       }}>
-      <Stack.Screen name="Places" component={PlacesRecommendScreen} />
-      <Stack.Screen name="Courses" component={CoursesRecommendScreen} />
+      <Stack.Screen name="Recommend" component={RecommendScreen} />
+      <Stack.Screen name="CourseEdit" component={CourseEditScreen} />
     </Stack.Navigator>
   );
 }
