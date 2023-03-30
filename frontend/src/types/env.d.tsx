17284a01f817e5d3b701b44aa050e338bdb1bffd
Edward <qwe_asd13579@naver.com>
2023-03-30 22:02:19 +0900

    refactor: 네이버 맵 API 키 환경변수 분리
    related to: #S08P22A408-323

--- frontend/src/types/env.d.tsx
+++ frontend/src/types/env.d.tsx
@@ -1,4 +1,6 @@
 declare module '@env' {
   export const REST_API_URL: string;
   export const GOOGLE_MAPS_API_KEY: string;
+  export const NAVER_MAPS_API_ID: string;
+  export const NAVER_MAPS_API_KEY: string;
 }
