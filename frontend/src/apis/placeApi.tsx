6ff050c51e1dee14b5fb0afa2fe6d4dc5cf88904
Edward <qwe_asd13579@naver.com>
2023-03-31 00:17:57 +0900

    feat: 장소, 코스 RTK Query API 구현
    related to: #S08P22A408-322

---
+++ frontend/src/apis/placeApi.tsx
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { REST_API_URL } from "@env";
+import { Category, Place } from "../types";
+
+export interface PlaceSearch {
+    regions: string[];
+    category: Category;
+    size: number;
+}
+
+export const placeApi = createApi({
+    reducerPath: 'placeApi',
+    baseQuery: fetchBaseQuery({baseUrl: REST_API_URL+'/place'}),
+    tagTypes: ['Places'],
+    endpoints: (builder) => ({
+        getPlaces: builder.query<Place[], PlaceSearch>({
+            query: (filter) => ({
+                url: '/',
+                params: filter
+            }),
+            providesTags: ['Places']
+        })
+    })
+})
+
+export const {useGetPlacesQuery} = placeApi;
