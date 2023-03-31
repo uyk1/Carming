e21b60cb3edbbb27b7540c0bf56a45ccbf8f614d
ShinKwang <sklee0206@naver.com>
2023-03-31 13:20:26 +0900

    fix: error handling by controller advice

--- backend/src/main/java/com/carming/backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/carming/backend/common/config/SecurityConfig.java
@@ -1,29 +1,19 @@
 package com.carming.backend.common.config;
 
 import com.carming.backend.common.filter.AuthEntryPoint;
-//import com.carming.backend.common.filter.JwtAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.carming.backend.common.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Autowired
-    @Qualifier("delegatedAuthenticationEntryPoint")
-    private AuthenticationEntryPoint authEntryPoint;
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,15 +25,13 @@ public class SecurityConfig {
                 .csrf().disable()
                 .formLogin().disable()
                 .httpBasic().disable()
-                .exceptionHandling().authenticationEntryPoint(authEntryPoint)
+                .exceptionHandling().authenticationEntryPoint(new AuthEntryPoint())
         .and()
 //                .authorizeRequests()
 //                        .antMatchers("/api/member/**", "/api/auth/**", "/api/tags").permitAll()
 //                        .anyRequest().authenticated();
-//                .and()
                 .authorizeRequests()
-                .anyRequest().permitAll()
-                .and().exceptionHandling().authenticationEntryPoint(authEntryPoint);
+                .anyRequest().permitAll();
 
 //        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
@@ -55,10 +43,10 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public JwtAuthenticationFilter jwtAuthenticationFilter() {
-//        return new JwtAuthenticationFilter();
-//    }
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
