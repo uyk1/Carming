2206b5aaf6f6458a11b81e59d4e62931cb75318f
Edward <qwe_asd13579@naver.com>
2023-04-02 18:15:39 +0900

    feat: 차량호출 화면 redux, RTK query 연동

--- frontend/src/screens/CarCallScreen.tsx
+++ frontend/src/screens/CarCallScreen.tsx
@@ -1,59 +1,112 @@
 import {CompositeScreenProps} from '@react-navigation/native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {StyleSheet, Dimensions} from 'react-native';
 import {useTheme} from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import styled from 'styled-components';
+import {
+  useGetCurrentCarPositionQuery,
+  useGetGlobalPathQuery,
+  useSetDriveStartStatusMutation,
+  useSetIsDestinationMutation,
+} from '../apis/journeyApi';
 import {CarCallInfoCard, CustomButton, CustomMapView} from '../components';
 import {iconPlace} from '../components/MapMarker';
 import {L3_TotalJourneyStackParamList} from '../navigations/L3_TotalJourneyStackNavigator';
 import {L4_JourneyStackParamList} from '../navigations/L4_JourneyStackNavigator';
-import {placesToCoordinates} from '../utils';
+import {calcArrivalTime, coordinateToIconPlace} from '../utils';
+import {useCheckIsDestinationQuery} from '../apis/journeyApi';
+import {useDispatch} from 'react-redux';
+import {setCurrentIdx} from '../redux/slices/journeySlice';
 
 export type CarCallScreenProps = CompositeScreenProps<
   NativeStackScreenProps<L4_JourneyStackParamList, 'CarCall'>,
   NativeStackScreenProps<L3_TotalJourneyStackParamList>
 >;
 
-const iconPlaces: iconPlace[] = [
-  {
-    iconName: 'hail',
-    lon: 126.97944891,
-    lat: 37.57171765,
-  },
-  {
-    iconName: 'taxi',
-    lon: 126.97844891,
-    lat: 37.57271765,
-  },
-  {
-    iconName: 'map-marker',
-    lon: 126.97744891,
-    lat: 37.57371765,
-  },
-];
-
 const CarCallScreen: React.FC<CarCallScreenProps> = ({navigation, route}) => {
   const theme = useTheme();
-  const routeCoordinates = placesToCoordinates(iconPlaces);
-  const [buttonAbled, setButtonAbled] = useState<boolean>(true);
+  const dispatch = useDispatch();
+  const {start: startCoordinate, end: endCoordinate} = route.params;
+
+  const startPlace = coordinateToIconPlace('map-marker', startCoordinate);
+  const endPlace = coordinateToIconPlace('hail', endCoordinate);
+
+  const [buttonAbled, setButtonAbled] = useState<boolean>(false);
+  const [currentCarPlace, setCurrentCarPlace] = useState<iconPlace>(
+    coordinateToIconPlace('taxi', startCoordinate),
+  );
+  const [arrivalTime, setArrivalTime] = useState<{
+    hour: number;
+    minute: number;
+  }>(calcArrivalTime(startCoordinate, endCoordinate));
+
+  const {data: currentCarCoordinate} = useGetCurrentCarPositionQuery(
+    undefined,
+    {
+      pollingInterval: 1000,
+    },
+  );
+  const {data: globalPath} = useGetGlobalPathQuery();
+  const {data: isDestination} = useCheckIsDestinationQuery(undefined, {
+    pollingInterval: 1000,
+  });
+  const [setDriveStartStatus] = useSetDriveStartStatusMutation();
+  const [setIsDestination] = useSetIsDestinationMutation();
+
+  useEffect(() => {
+    if (currentCarCoordinate !== undefined) {
+      const {latitude: lat, longitude: lon} = currentCarCoordinate;
+      setCurrentCarPlace({...currentCarPlace, lat, lon});
+      setArrivalTime(calcArrivalTime(currentCarCoordinate, endCoordinate));
+    }
+  }, [currentCarCoordinate]);
+
+  useEffect(() => {
+    if (globalPath !== undefined) {
+    }
+  }, [globalPath]);
+
+  useEffect(() => {
+    if (isDestination !== undefined) {
+      setButtonAbled(isDestination);
+    }
+  }, [isDestination]);
+
+  const completeBoardBtnPressed = () => {
+    setDriveStartStatus(1);
+    setIsDestination(0);
+    dispatch(setCurrentIdx(1));
+    navigation.replace('CarMove');
+  };
 
   return (
     <StyledSafeAreaView>
       <CarCallInfoCard
-        statusText={'카밍카가 출발했어요 '}
-        infoText={'14분 내로 도착할 예정이에요 '}
+        statusText={
+          isDestination ? '카밍카가 도착했어요.' : '카밍카가 출발했어요.'
+        }
+        infoText={
+          isDestination
+            ? '탑승완료 버튼을 눌러주세요.'
+            : `${arrivalTime.hour !== 0 ? `${arrivalTime.hour}시간` : ''} ${
+                arrivalTime.hour === 0 && arrivalTime.minute === 0
+                  ? '곧'
+                  : `${arrivalTime.minute}분 내로`
+              } 도착할 예정이에요.`
+        }
+        imageActive={!isDestination}
       />
       <CustomMapView
-        places={iconPlaces}
+        places={[startPlace, endPlace, currentCarPlace]}
         viewStyle={{flex: 1}}
         latitudeOffset={0}
-        routeCoordinates={routeCoordinates}
+        routeCoordinates={globalPath}
       />
       <CustomButton
         text={'차량 탑승 완료'}
-        onPress={() => {}}
+        onPress={() => completeBoardBtnPressed()}
         disabled={!buttonAbled}
         buttonStyle={{
           ...styles.buttonStyle,
