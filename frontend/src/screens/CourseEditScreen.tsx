6a795364413b281e3c1265bf438121327c13c376
Edward <qwe_asd13579@naver.com>
2023-04-02 18:17:23 +0900

    feat: 코스 순서변경 화면 Redis 연동

--- frontend/src/screens/CourseEditScreen.tsx
+++ frontend/src/screens/CourseEditScreen.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Dimensions, StyleSheet, Text, View} from 'react-native';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {IconButton, useTheme} from 'react-native-paper';
 import LinearGradient from 'react-native-linear-gradient';
@@ -12,10 +12,26 @@ import {RootState} from '../redux/store';
 import DraggableFlatList from 'react-native-draggable-flatlist';
 import {CourseEditListItem, CustomButton, CustomMapView} from '../components';
 import {CompositeScreenProps} from '@react-navigation/native';
-import {L3_TotalJourneyStackParamList} from '../navigations/L3_TotalJourneyStackNavigator';
 import {ReactNativeModal as Modal} from 'react-native-modal';
-import {calcTime, pathToCoordinates, placesToCoordinates} from '../utils';
+import {
+  calcTime,
+  coordinateToIconPlace,
+  pathToCoordinates,
+  placesToCoordinates,
+} from '../utils';
 import {callNaverDirectionApi} from '../apis/mapApi';
+import {
+  useGetCurrentCarPositionQuery,
+  useSetDriveStartStatusMutation,
+  useSetIsDestinationMutation,
+} from '../apis/journeyApi';
+import {
+  AlertNotificationRoot,
+  ALERT_TYPE,
+  Toast,
+} from 'react-native-alert-notification';
+import {L3_TotalJourneyStackParamList} from '../navigations/L3_TotalJourneyStackNavigator';
+import {setJourneyPlaceList} from '../redux/slices/journeySlice';
 
 export type CourseEditScreenProps = CompositeScreenProps<
   NativeStackScreenProps<L4_CourseCreateStackParamList, 'CourseEdit'>,
@@ -23,30 +39,40 @@ export type CourseEditScreenProps = CompositeScreenProps<
 >;
 
 const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
+const currentClientPosition: Coordinate = {
+  latitude: 37.503353299999844,
+  longitude: 127.04569145393688,
+};
 
 const CourseEditScreen: React.FC<CourseEditScreenProps> = ({
   navigation,
   route,
 }) => {
   const theme = useTheme();
+  const dispatch = useDispatch();
   const placeCart = useSelector((state: RootState) => state.place.placeCart);
   const courseCart = useSelector((state: RootState) => state.course.courseCart);
 
+  const clientPlace = coordinateToIconPlace('hail', currentClientPosition);
   const [coursePlaces, setCoursePlaces] = useState<Place[]>([]);
   const [routeModalVisible, setRouteModalVisible] = useState<boolean>(false);
   const [placeCoordinates, setPlaceCoordinates] = useState<Coordinate[]>([]);
   const [routeCoordinates, setRouteCoordinates] = useState<Coordinate[]>([]);
   const [routeInfo, setRouteInfo] = useState<any>({});
 
-  useEffect(() => {
-    setPlaceCoordinates(placesToCoordinates(coursePlaces));
-  }, [coursePlaces]);
+  const {data: currentCarPosition} = useGetCurrentCarPositionQuery();
+  const [setIsDestination] = useSetIsDestinationMutation();
+  const [setDriveStartStatus] = useSetDriveStartStatusMutation();
 
   useEffect(() => {
     route.params.recommendType === '0'
       ? setCoursePlaces([...placeCart])
       : setCoursePlaces([...courseCart]);
   }, [placeCart, courseCart, route]);
+
+  useEffect(() => {
+    setPlaceCoordinates(placesToCoordinates([clientPlace, ...coursePlaces]));
+  }, [coursePlaces]);
 
   const calcRoute = async () => {
     await callNaverDirectionApi(placeCoordinates)
@@ -58,107 +84,125 @@ const CourseEditScreen: React.FC<CourseEditScreenProps> = ({
       .catch(err => console.log('error :', err));
   };
 
+  const journeyStartBtnPressed = async () => {
+    if (currentCarPosition) {
+      dispatch(setJourneyPlaceList([clientPlace, ...coursePlaces]));
+      setDriveStartStatus(1);
+      setIsDestination(0);
+      navigation.navigate('Journey', {
+        screen: 'CarCall',
+        params: {start: currentCarPosition, end: currentClientPosition},
+      });
+    } else {
+      Toast.show({
+        type: ALERT_TYPE.WARNING,
+        title: '준비된 차량이 없습니다.',
+        textBody: '잠시 후에 다시 이용해 주세요.',
+      });
+    }
+    setRouteModalVisible(false);
+  };
+
   return (
-    <GradientBackground colors={['#70558e7a', '#df94c283', '#ffbdc1b0']}>
-      <SafeAreaView style={{flex: 1}}>
-        <StyledView
-          style={{justifyContent: 'space-between', flexDirection: 'row'}}>
-          <IconButton
-            icon="chevron-left"
-            size={30}
-            onPress={() => {
-              navigation.goBack();
-            }}
+    <AlertNotificationRoot>
+      <GradientBackground colors={['#70558e7a', '#df94c283', '#ffbdc1b0']}>
+        <SafeAreaView style={{flex: 1}}>
+          <StyledView
+            style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+            <IconButton
+              icon="chevron-left"
+              size={30}
+              onPress={() => {
+                navigation.goBack();
+              }}
+            />
+            <IconButton
+              icon="home"
+              size={30}
+              onPress={() => {
+                console.log('hello');
+              }}
+            />
+          </StyledView>
+          <TitleText>코스 짜기</TitleText>
+          <DescText>드래그 & 드롭으로 순서를 변경할 수 있어요!</DescText>
+          <DraggableFlatList
+            containerStyle={{padding: 20}}
+            data={coursePlaces}
+            onDragEnd={({data}) => setCoursePlaces(data)}
+            keyExtractor={item => item.id.toString()}
+            renderItem={CourseEditListItem}
           />
-          <IconButton
-            icon="home"
-            size={30}
-            onPress={() => {
-              console.log('hello');
-            }}
-          />
-        </StyledView>
-        <TitleText>코스 짜기</TitleText>
-        <DescText>드래그 & 드롭으로 순서를 변경할 수 있어요!</DescText>
-        <DraggableFlatList
-          containerStyle={{padding: 20}}
-          data={coursePlaces}
-          onDragEnd={({data}) => setCoursePlaces(data)}
-          keyExtractor={item => item.id.toString()}
-          renderItem={CourseEditListItem}
-        />
-        <StyledView style={{justifyContent: 'center'}}>
-          <CustomButton
-            text={'경로 계산하기'}
-            onPress={async () => {
-              await calcRoute();
-              setRouteModalVisible(true);
-            }}
-            buttonStyle={{
-              ...styles.button,
-              backgroundColor: theme.colors.surfaceVariant,
-            }}
-            textStyle={styles.buttonText}
-          />
-        </StyledView>
-      </SafeAreaView>
-      <Modal
-        testID={'modal'}
-        isVisible={routeModalVisible}
-        onSwipeComplete={() => setRouteModalVisible(false)}
-        swipeDirection={['down']}
-        style={styles.view}>
-        <StyledView style={styles.modalView}>
-          <CustomMapView
-            places={coursePlaces}
-            viewStyle={{
-              width: screenWidth * 0.8,
-              height: screenWidth * 0.8,
-            }}
-            useIndex={true}
-            routeCoordinates={routeCoordinates}
-          />
-          <ModalInfoText>
-            예상 이동시간 : 총{' '}
-            {calcTime(routeInfo.duration).hour
-              ? calcTime(routeInfo.duration).hour + '시간 '
-              : ' '}
-            {calcTime(routeInfo.duration).minute}분
-          </ModalInfoText>
-          <ModalInfoText>
-            예상 비용 : 총{' '}
-            {routeInfo.taxiFare + routeInfo.fuelPrice + routeInfo.tollFare}원
-          </ModalInfoText>
-          <ModalPriceText>
-            (대여료 {routeInfo.taxiFare}원 + 유류비 {routeInfo.fuelPrice}원 +
-            통행 요금 {routeInfo.tollFare}원)
-          </ModalPriceText>
-          <View style={{flexDirection: 'row'}}>
+          <StyledView style={{justifyContent: 'center'}}>
             <CustomButton
-              text={'여정 시작하기'}
+              text={'경로 계산하기'}
               onPress={async () => {
-                navigation.navigate('Journey');
-                setRouteModalVisible(false);
+                await calcRoute();
+                setRouteModalVisible(true);
               }}
               buttonStyle={{
-                ...styles.modalButton,
+                ...styles.button,
                 backgroundColor: theme.colors.surfaceVariant,
               }}
-              textStyle={styles.modalButtonText}
+              textStyle={styles.buttonText}
             />
-            <CustomButton
-              text={'취소하기'}
-              onPress={() => setRouteModalVisible(false)}
-              buttonStyle={{
-                ...styles.modalButton,
-                backgroundColor: theme.colors.tertiary,
+          </StyledView>
+        </SafeAreaView>
+        <Modal
+          testID={'modal'}
+          isVisible={routeModalVisible}
+          onSwipeComplete={() => setRouteModalVisible(false)}
+          swipeDirection={['down']}
+          style={styles.view}>
+          <StyledView style={styles.modalView}>
+            <CustomMapView
+              places={[clientPlace, ...coursePlaces]}
+              viewStyle={{
+                width: screenWidth * 0.8,
+                height: screenWidth * 0.8,
               }}
-              textStyle={styles.modalButtonText}
+              useIndex={true}
+              routeCoordinates={routeCoordinates}
             />
-          </View>
-        </StyledView>
-      </Modal>
-    </GradientBackground>
+            <ModalInfoText>
+              예상 이동시간 : 총{' '}
+              {calcTime(routeInfo.duration).hour
+                ? calcTime(routeInfo.duration).hour + '시간 '
+                : ' '}
+              {calcTime(routeInfo.duration).minute}분
+            </ModalInfoText>
+            <ModalInfoText>
+              예상 비용 : 총{' '}
+              {routeInfo.taxiFare + routeInfo.fuelPrice + routeInfo.tollFare}원
+            </ModalInfoText>
+            <ModalPriceText>
+              (대여료 {routeInfo.taxiFare}원 + 유류비 {routeInfo.fuelPrice}원 +
+              통행 요금 {routeInfo.tollFare}원)
+            </ModalPriceText>
+            <View style={{flexDirection: 'row'}}>
+              <CustomButton
+                text={'여정 시작하기'}
+                onPress={() => journeyStartBtnPressed()}
+                buttonStyle={{
+                  ...styles.modalButton,
+                  backgroundColor: theme.colors.surfaceVariant,
+                }}
+                textStyle={styles.modalButtonText}
+              />
+              <CustomButton
+                text={'취소하기'}
+                onPress={() => setRouteModalVisible(false)}
+                buttonStyle={{
+                  ...styles.modalButton,
+                  backgroundColor: theme.colors.tertiary,
+                }}
+                textStyle={styles.modalButtonText}
+              />
+            </View>
+          </StyledView>
+        </Modal>
+      </GradientBackground>
+    </AlertNotificationRoot>
   );
 };
 
