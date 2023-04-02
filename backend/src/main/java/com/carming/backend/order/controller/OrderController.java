a6ec26191b7cbd75b317deb63a6470bda450c69f
Edward <qwe_asd13579@naver.com>
2023-04-02 17:56:49 +0900

    feat: Redis get, set DTO & 컨트롤러 생성

--- backend/src/main/java/com/carming/backend/order/controller/OrderController.java
+++ backend/src/main/java/com/carming/backend/order/controller/OrderController.java
@@ -1,9 +1,11 @@
 package com.carming.backend.order.controller;
 
 import com.carming.backend.order.domain.request.DestinationDto;
+import com.carming.backend.order.domain.request.RedisDTO;
 import com.carming.backend.order.domain.response.IsArrival;
 import com.carming.backend.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,17 @@ import java.util.List;
 public class OrderController {
 
     private final OrderService orderService;
+
+    @PostMapping("")
+    public ResponseEntity<Void> save(@RequestBody RedisDTO request) {
+        orderService.setValueByKey(request.getKey(), request.getValue());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<String> get(@RequestParam String key) {
+        return ResponseEntity.ok(orderService.getValueByKey(key));
+    }
 
     @PostMapping("/dest")
     public ResponseEntity<Void> saveDestination(@RequestBody DestinationDto request) {
