55a296295bdfdb301eedd3c55ae8bd7b2af625bb
김인중 <qwe_asd13579@naver.com>
2023-04-03 17:45:03 +0900

    feat: 리뷰화면 api 연동
    related to: #S08P22A408-336

--- frontend/src/screens/ReviewWirteScreen.tsx
+++ frontend/src/screens/ReviewWirteScreen.tsx
@@ -3,121 +3,118 @@ import {CustomButton, ReviewCourseItem, ReviewPlaceItem} from '../components';
 import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../redux/store';
 import {filterRealPlace, placesToPlaceReviewRequests} from '../utils';
-import {
-  Category,
-  Course,
-  CourseReviewRequest,
-  Place,
-  PlaceReviewRequest,
-} from '../types';
-import {useEffect, useState} from 'react';
-import {useGetTagsQuery} from '../apis/tagApi';
-import {setTagList} from '../redux/slices/tagSlice';
+import {ReviewRequest} from '../types';
+import {useEffect} from 'react';
 import styled from 'styled-components';
 import {setCourseReview, setplaceReviews} from '../redux/slices/reviewSlice';
-import {useTheme} from 'react-native-paper';
+import {ActivityIndicator, useTheme} from 'react-native-paper';
+import {useCheckCourseExistQuery} from '../apis/courseApi';
+import {useRegistReviewMutation} from '../apis/reviewApi';
+import {CompositeScreenProps} from '@react-navigation/native';
+import {DrawerScreenProps} from '@react-navigation/drawer';
+import {L2_AppDrawerParamList} from '../navigations/L2_AppDrawerNavigator';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {L4_JourneyEndStackParamList} from '../navigations/L4_JourneyEndStackNavigator';
+import {ScrollView} from 'react-native-gesture-handler';
+import {ALERT_TYPE, Toast} from 'react-native-alert-notification';
 
-const ReviewWirteScreen = () => {
+type ReviewWirteScreenProps = CompositeScreenProps<
+  NativeStackScreenProps<L4_JourneyEndStackParamList, 'Review'>,
+  DrawerScreenProps<L2_AppDrawerParamList>
+>;
+
+const ReviewWirteScreen: React.FC<ReviewWirteScreenProps> = ({navigation}) => {
   const theme = useTheme();
   const dispatch = useDispatch();
   const placeList = useSelector((state: RootState) =>
     filterRealPlace(state.journey.placeList),
   );
-  const {placeReviews, courseReview, courseTitle} = useSelector(
+  const {placeReviews, courseReview} = useSelector(
     (state: RootState) => state.review,
   );
 
+  const {data: isCourseExist, isFetching: isCourseExistFetching} =
+    useCheckCourseExistQuery(placeList.map(place => place.id));
+
+  const [registReview, registReviewStatus] = useRegistReviewMutation();
+
   useEffect(() => {
     dispatch(setplaceReviews(placesToPlaceReviewRequests(placeList)));
-    dispatch(setCourseReview({...courseReview, courseId: 10}));
   }, []);
 
-  const {data: tagLists} = useGetTagsQuery();
+  useEffect(() => {
+    if (isCourseExist && !isCourseExist.newCourse) {
+      const {courseId} = isCourseExist;
+      dispatch(setCourseReview({...courseReview, courseId}));
+    }
+  }, [isCourseExist]);
 
   useEffect(() => {
-    if (tagLists !== undefined) dispatch(setTagList(tagLists));
-  }, [tagLists]);
+    if (registReviewStatus.isSuccess) {
+      Toast.show({
+        type: ALERT_TYPE.SUCCESS,
+        title: 'Success',
+        textBody: '리뷰가 등록되었습니다.',
+      });
+      navigation.navigate('Main');
+    } else if (registReviewStatus.isError) {
+      Toast.show({
+        type: ALERT_TYPE.WARNING,
+        title: '리뷰가 등록되지 않았습니다.',
+        textBody: '잠시 후에 다시 시도해주세요.',
+      });
+    }
+  }, [registReviewStatus]);
+
+  const submitBtnPressed = () => {
+    const newReviewRequest: ReviewRequest = {
+      placeReviews,
+      courseReview,
+    };
+    registReview(newReviewRequest);
+  };
+
+  const reviewScreenContent = () => {
+    if (isCourseExistFetching) {
+      return (
+        <ActivityIndicator
+          size={'large'}
+          style={{flex: 1}}
+          animating={true}
+          color={theme.colors.onPrimary}
+        />
+      );
+    } else {
+      return (
+        <>
+          <TitleText style={{color: theme.colors.surfaceVariant}}>
+            리뷰 작성하기
+          </TitleText>
+          {placeList.map((place, index) => (
+            <ReviewPlaceItem key={place.id} place={place} index={index + 1} />
+          ))}
+          <ReviewCourseItem isFirstTime={isCourseExist?.newCourse ?? true} />
+          <CustomButton
+            text={'완료'}
+            onPress={() => submitBtnPressed()}
+            buttonStyle={{
+              ...styles.button,
+              backgroundColor: theme.colors.surfaceVariant,
+            }}
+            textStyle={styles.buttonText}
+          />
+        </>
+      );
+    }
+  };
 
   return (
     <StyledSafeAreaView>
-      <ScreenContainer>
-        <TitleText style={{color: theme.colors.surfaceVariant}}>
-          리뷰 작성하기
-        </TitleText>
-        {placeList.map((place, index) => (
-          <ReviewPlaceItem key={place.id} place={place} index={index + 1} />
-        ))}
-        <ReviewCourseItem isFirstTime={false} />
-        <CustomButton
-          text={'완료'}
-          onPress={() => {}}
-          buttonStyle={{
-            ...styles.button,
-            backgroundColor: theme.colors.surfaceVariant,
-          }}
-          textStyle={styles.buttonText}
-        />
-      </ScreenContainer>
+      <StyledScrollView>
+        <ScreenContainer>{reviewScreenContent()}</ScreenContainer>
+      </StyledScrollView>
     </StyledSafeAreaView>
   );
-};
-
-const dummyPlace: Place = {
-  id: 4,
-  name: '카페공명',
-  tel: '070-8869-6304',
-  category: Category.카페,
-  lon: 126.92635262,
-  lat: 37.5598709,
-  region: '마포구',
-  ratingCount: 127,
-  ratingSum: 470,
-  keyword: ['공부하기좋은', '베이커리카페', '북카페', '이색카페'],
-  image:
-    'https://d1ldvf68ux039x.cloudfront.net/thumbs/frames/audio/2211/71249/122x92_q95.jpg',
-};
-const dummyCourse: Course = {
-  name: '노잼은 아닌 코스',
-  regions: ['노원구', '은평구', '관악구'],
-  places: [
-    {
-      id: 1,
-      name: '어반플랜트 합정',
-      lon: 126.9171881,
-      lat: 37.54789417,
-      image: 'http://t1.daumcdn.net/localfiy/searchregister_1152174062',
-      ratingCount: 275,
-      ratingSum: 1155,
-    },
-    {
-      id: 2,
-      name: '피오니 홍대점',
-      lon: 126.91976584,
-      lat: 37.55008538,
-      image: 'http://t1.daumcdn.net/cfile/276E0A4C550A37652A',
-      ratingCount: 214,
-      ratingSum: 813,
-    },
-    {
-      id: 3,
-      name: '프릳츠 도화점',
-      lon: 126.94907049,
-      lat: 37.54101958,
-      image:
-        'http://t1.kakaocdn.net/fiy_reboot/place/0E03C7048DF94AB187582A216C7CA500',
-      ratingCount: 288,
-      ratingSum: 1123,
-    },
-    {
-      id: 4,
-      name: '카페공명',
-      lon: 126.92635262,
-      lat: 37.5598709,
-      image: 'http://t1.daumcdn.net/localfiy/searchregister_1919726515',
-      ratingCount: 127,
-      ratingSum: 470,
-    },
-  ],
 };
 
 const styles = StyleSheet.create({
@@ -152,6 +149,10 @@ const TitleText = styled(Text)`
   font-weight: bold;
   width: 100%;
   margin: 20px 0px 40px 0px;
+`;
+
+const StyledScrollView = styled(ScrollView)`
+  flex: 1;
 `;
 
 export default ReviewWirteScreen;
