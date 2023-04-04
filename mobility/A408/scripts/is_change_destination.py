17381c8a3a9341458b50d9e12923ffea5fc41bd5
unknown <06___what@naver.com>
2023-04-05 08:33:59 +0900

    Fix

--- mobility/A408/scripts/is_change_destination.py
+++ mobility/A408/scripts/is_change_destination.py
@@ -1,39 +1,19 @@
 #!/usr/bin/env python
 
-import rospy
-from morai_msgs.msg import EgoVehicleStatus
-from morai_msgs.srv import MoraiEventCmdSrv
 import redis
 import os
+import time
 
-redis_client = redis.Redis(host='j8a408.p.ssafy.io', port=6379, db=0, password='carming123')
+redis_client = redis.StrictRedis(host='j8a408.p.ssafy.io', port=6379, db=0, password='carming123')
 
-class Realtime_listener:
-    def __init__(self):
-        rospy.init_node("is_change_destination", anonymous=True)
+# get the value of 'destination_coordinate' key
+destination = redis_client.get('destination_coordinate')
 
-        # subscribe to a Redis channel
-        pubsub = redis_client.pubsub()
-        pubsub.subscribe('destination_coordinate')
-
-        self.data = {}
-        rate = rospy.Rate(2) # 2 times / 1 sec
-
-        while not rospy.is_shutdown():
-            for message in pubsub.listen():
-                # process the message when there is a new message
-                if message['type'] == 'message':
-                    self.data = eval(message['data']) # convert string to dictionary
-                    print("-----------------------")
-                    # execute roslaunch command
-                    os.system("roslaunch ssafy_2 Adaptive_Cruise_Control.launch")
-            rate.sleep()
-
-
-
-if __name__ == "__main__":
-    try:
-        start = Realtime_listener()
-
-    except rospy.ROSInterruptException:
-        pass
+while True:
+    # get the value of 'destination_coordinate' key
+    now_destination = redis_client.get('destination_coordinate')
+    if now_destination != destination:
+        destination = now_destination
+        os.system("roslaunch ssafy_2 Adaptive_Cruise_Control.launch")
+    
+    time.sleep(1)
